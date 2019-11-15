306af94578ac7ea272dd7efccc270ff7cabde839
whhlulu <1562891608@qq.com>
2019-11-15 23:24:23 +0800

    init

---
+++ siteinfo.js
@@ -0,0 +1,10 @@
+var t = {
+    acid: -1,
+    version: "3.1.49",
+  siteroot: "https://bm.0527888.cn",
+  apiroot: "https://bm.0527888.cn/web/index.php?store_id=1&r=api/"
+	//提示  只需要改域名和storeid即可
+
+};
+
+module.exports = t;
